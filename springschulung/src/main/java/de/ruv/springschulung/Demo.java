8ec45573f5f668acf256908ffbc1b078fb944177
limagoHub <limagoHub@limago.de>
2021-04-13 12:23:52 +0200

    properties

--- springschulung/src/main/java/de/ruv/springschulung/Demo.java
+++ springschulung/src/main/java/de/ruv/springschulung/Demo.java
@@ -5,6 +5,7 @@ import javax.annotation.PreDestroy;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Repository;
@@ -15,17 +16,22 @@ import org.springframework.stereotype.Service;
 //@Scope("prototype")
 public class Demo {
 	
+	//@Value("${Demo.wagner}")
+	private String message = "########################## Hallo";
+	
+	
 	//@Autowired
 	private final Translator translator;
 	
 	//@Autowired
 	public Demo(final @Qualifier ("upper") Translator translator) {
 		this.translator = translator;
-		System.out.println(translator.translate("########################## Hallo"));
+		System.out.println(translator.translate(message));
 	}
 	
 	@PostConstruct
 	public void run() {
+		System.out.println(translator.translate(message));
 		System.out.println(translator.translate("Ich renne..........."));
 	}
 	
